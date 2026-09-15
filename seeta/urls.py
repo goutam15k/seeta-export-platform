96dbb19fb8ab1f60fde3985b6e36b98cafeb7a8c
goutam15k <goutamchoudhary15k@gmail.com>
2026-09-15 21:29:12 +0530

    Import serve function in urls.py for media files

--- seeta/urls.py
+++ seeta/urls.py
@@ -3,6 +3,7 @@ from django.urls import path, re_path
 from myapp import views 
 from django.conf import settings 
 from django.conf.urls.static import static
+from django.views.static import serve
 
 urlpatterns = [
     path('admin/', admin.site.urls),
